9a7bcfb8b75452fdb405d3f80dab5f9f625dc8d5
NimayRKumar <nimay.r.kumar@gmail.com>
2024-08-09 12:23:32 -0400

    final commit

---
+++ src/words/g3/Group3Player.java
@@ -0,0 +1,206 @@
+
+package words.g3;
+
+import words.core.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+import java.util.logging.Level;
+
+
+public class Group3Player extends Player {
+    Map<Integer, Map<Letter, Integer>> playerCounts;             //format: Map <PlayerID <letter_type, num_letters_of_type>
+    Map<Letter, Integer> absoluteCounts;                         //format: Map <Letter, num_instances_on_board>
+    int totalPlayed;
+    ArrayList<String> vowels;
+    ArrayList<String> easyConst; // value of 4 or fewer
+    ArrayList<String> hardConst; // value of 5 or higher
+    String[] sortWords;
+
+    public Group3Player() {
+        playerCounts = new HashMap<>();
+        absoluteCounts = new HashMap<>();
+        totalPlayed = 0;
+        vowels = new ArrayList<>(Arrays.asList("A", "E", "I", "O", "U"));
+        easyConst = new ArrayList<>(Arrays.asList("B", "C", "D", "F", "G", "H", "L", "M", "N", "P", "R", "S", "T", "V", "W", "Y"));
+        hardConst = new ArrayList<>(Arrays.asList("J", "K", "Q", "X", "Z"));
+
+    }
+
+    protected void initializeSort() {
+        String line = null;
+        ArrayList<String> wtmp = new ArrayList<>(55000);
+        try {
+            BufferedReader r = new BufferedReader(new FileReader("src/words/g3/cleaned.txt"));
+            while (null != (line = r.readLine())) {
+                String newline = new String(line.trim());
+                newline = newline.replaceAll("\\d+", "");
+                wtmp.add(new String(line.trim()));
+            }
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "An error occurred.", e);
+        }
+        sortWords = wtmp.toArray(new String[0]);
+    }
+    @Override
+    public void startNewRound(SecretState secretstate){
+        totalPlayed = 0;
+        myLetters.clear(); // clear the letters that I have
+        // this puts the secret letters into the currentLetters List
+        myLetters.addAll(secretstate.getSecretLetters().stream().map(Letter::getCharacter).toList());
+
+        playerLetters.clear(); // clear the letters that all the players have
+        for (int i = 0; i < numPlayers; i++) {
+            playerLetters.add(new LinkedList<Character>()); // initialize each player's list of letters
+        }
+        /*
+        Note that although the letters that I have will be in the playerLetters List, the playerLetters
+        List doesn't include my secret letters.
+         */
+    }
+
+    @Override
+    public void startNewGame(int playerID, int numPlayers) {
+        myID = playerID; // store my ID
+        initializeSort();
+
+        initializeWordlist(); // read the file containing all the words
+
+        this.numPlayers = numPlayers; // so we know how many players are in the game
+    }
+
+    private void recordLetter(Letter letter, int ownerID) {
+
+        //check data
+        if (letter == null) return;     //case letter is invalid
+        if (ownerID < 0) return;       //case no one won this bid
+
+        //if here, letter is valid
+
+        // Add to the player count
+        if (!playerCounts.containsKey(ownerID)) playerCounts.put(ownerID, new HashMap<Letter, Integer>());      //case never seen this player win
+        else if (!playerCounts.get(ownerID).containsKey(letter)) playerCounts.get(ownerID).put(letter, 1);      //case this player has no letters of this type
+        else playerCounts.get(ownerID).put(letter, (playerCounts.get(ownerID).get(letter)+1));                  //case this player already has a letter of this type
+
+        //Add to the absolute count
+        if(!absoluteCounts.containsKey(letter)) absoluteCounts.put(letter, 1);
+        else absoluteCounts.put(letter, absoluteCounts.get(letter) + 1);
+
+        //add to total seen
+        totalPlayed++;
+
+    }
+
+
+
+
+
+
+    public static boolean containsAllLetters(String str, List<Character> letters) {
+        for (char letter : letters) {
+            if (str.indexOf(letter) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<Character> sorter( List<Character> letters){
+        List<Character> sortedList = new ArrayList<>(letters);
+        Collections.sort(sortedList);
+        return sortedList;
+    }
+
+
+    @Override
+    public int bid(Letter bidLetter, List<PlayerBids> playerBidList, int totalRounds, ArrayList<String> playerList, SecretState secretstate, int playerID) {
+        int lastIndex = playerBidList.size() - 1;
+        if (playerBidList.size() > 0) recordLetter(playerBidList.get(lastIndex).getTargetLetter(), playerBidList.get(lastIndex).getWinnerID());
+
+        if(totalPlayed < 3){
+            return 3;
+        }
+
+        // THIS checks if we have a valid word of 7 or more letters. at this point, stop betting.
+        // IMPROVEMENT made: if new letter adds to value of our existing word... than can bid
+        String word = returnWord();
+        if (word.length() >= 7){
+            int c_count = 0;
+            for(String w: sortWords){
+                if((w.length() == 7) && (containsAllLetters(w, myLetters))){
+                    if(w.contains(String.valueOf(bidLetter.getCharacter()))){
+                        c_count ++;
+                    }
+                }
+                if (c_count >=1){
+                    return 5;
+                }
+
+            }
+            return 0;
+        }
+
+
+        //need to consider rounds left
+        int roundsLeft = numPlayers * 8 - totalPlayed;
+        // strategy: rounds left + number of letters > 11 to bid heavily to get a 7-letter word
+        // aka in the >= 4 section
+        //reasoning: we will assume some we will be outbid despite big bets, or some letters dont work
+        //thus, we will only pursue 7 letters if enough time
+
+
+        // THIS: if we have 4 or more letters
+        //count all the 7 (or more) letter words CONTAINING the letters that we already have
+        //then, for all those long words ^, count how many have the new letter
+        // then, bid appropriately based on how much that letter will help (probability of it being in a longer water)
+
+        // TO DO: test these bids and thesholds => mitali just did it randomly
+        // POSSIBLE LIMITATION: can all but one of these letters work?
+
+
+        if((myLetters.size() >= 4) && (1.5*(7-myLetters.size())< roundsLeft)){ //&& (
+            int w_count = 0;
+            int c_count = 0;
+            for(String w: sortWords){
+                if((w.length() >= 7) && (containsAllLetters(w, myLetters))){
+                    w_count ++;
+                    if(w.contains(String.valueOf(bidLetter.getCharacter()))){
+                        c_count ++;
+                    }
+                }
+
+            }
+            if ((double) c_count / w_count > 0.9){
+                return 10;
+            }
+            if ((double) c_count / w_count > 0.7){
+                return 8;
+            }
+            if ((double) c_count / w_count > 0.5){
+                return 5;
+            }
+            return 1;
+
+        }
+
+
+
+
+        // if we do not have 4 letters, bid on letterswith following strat:
+        // we wnt vowels and easy constinents
+        //if we cant get them, make the other teams pay for them!
+
+        if (vowels.contains(String.valueOf(bidLetter.getCharacter()))){
+            return 6;
+        }
+        if (easyConst.contains(String.valueOf(bidLetter.getCharacter()))){
+            return 6;
+        }
+        if (hardConst.contains(String.valueOf(bidLetter.getCharacter()))){
+            return 4;
+        }
+
+        return 0;
+    }
+}
--- src/words/g5/Group5Player.java
+++ src/words/g5/Group5Player.java
@@ -3,111 +3,86 @@ package words.g5;
 import words.core.Letter;
 import words.core.PlayerBids;
 import words.core.SecretState;
-import words.core.Word;
 import words.core.ScrabbleValues;
 
-import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Group5Player extends words.core.Player {
-
-    private HashMap<Character, Double> freqInWords = new HashMap<>();
-    private HashMap<Character, Double> freqInLetters = new HashMap<>();
-
-    private HashMap<Character, Double> playerLetterFreq = new HashMap<>();
-
-    private int maxVowelBid = 8;
-    private int vowelBid = 2;
+    private int maxVowelBid = 12;
+    private int minVowelBid = 2;
+    private int vowelBid = 8;
 
     private boolean isVowel(char c) {
         return "AEIOUaeiou".indexOf(c) >= 0;
     }
 
-    private double countOccurrences(char c) {
-        double count = 0.0;
-        Word[] wordList = super.wordlist;
-
-        for (Word w : wordList) {
-            String wString = w.word;
-            for (int i=0; i<wString.length(); ++i) {
-                if (c == '?') {
-                    ++ count;
-                }
-                else if (c == wString.charAt(i)) {
-                    ++count;
-                }
-            }
-        }
-
-        return count;
-    }
-
-    private void initFreqMaps() {
-        char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-        double[] freq = {9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1};
-
-        double totalNumChars = countOccurrences('?');
-        for (int i=0; i<26; ++i) {
-            freqInLetters.put(chars[i], freq[i] / 98.0);
-            double charFreq = countOccurrences(chars[i]);
-            freqInWords.put(chars[i], charFreq / totalNumChars);
-        }
-    }
-
-    private int countLetterList(List<Character> letters, Character letter) {
+    private int countCharacterList(List<Character> letters, Character letter) {
         int count = 0;
-
         for (int i=0; i<letters.size(); ++i) {
-            if (letters.get(i) ==letter) {
+            if (letters.get(i) == letter) {
                 ++count;
             }
         }
+        return count;
+    }
 
+    private int countLetterArrayList(ArrayList<Letter> letters, Letter letter) {
+        int count = 0;
+        for (int i=0; i<letters.size(); ++i) {
+            if (letters.get(i).getCharacter() == letter.getCharacter()) {
+                ++count;
+            }
+        }
         return count;
     }
 
     private int countLetterArr(char[] chars, char letter) {
         int count = 0;
 
-        for (int i=0; i<chars.length; ++i) {
-            if (chars[i] ==letter) {
+        for (int i = 0; i < chars.length; ++i) {
+            if (chars[i] == letter) {
                 ++count;
             }
         }
-
         return count;
+    }
+
+    private double[] countVowelsAndConsonants() {
+        double numVowels = 0.0;
+        double numConsonants = 0.0;
+        for (Character c : myLetters) {
+            if (isVowel(c)) {
+                ++numVowels;
+            } else {
+                ++numConsonants;
+            }
+        }
+
+        return new double[]{numVowels, numConsonants};
     }
 
     @Override
     public int bid(Letter bidLetter, List<PlayerBids> playerBidList, int totalRounds, ArrayList<String> playerList, SecretState secretstate, int playerID) {
         String currentBest = returnWord();
 
-        //if we can already make a 7+ letter word, bid only 1
-        if (currentBest.length() >= 7) {
+        //if we can already make a 7+ letter word, bid 1
+        if (currentBest.length() >= 7 || secretstate.getScore() <= 30 || secretstate.getTotalLetters() >= 20) {
             return Math.min(1, secretstate.getScore());
         }
 
-        //if we have vowels and consonants in an unhealthy ratio, bid more for vowels
-        double numVowels = 0.0;
-        double numConsonants = 0.0;
-        for (Character c : myLetters) {
-            if (isVowel(c)) {
-                ++numVowels;
-            }
-            else {
-                ++numConsonants;
-            }
+        //if we have vowels and consonants in a suboptimal ratio, bid more for vowels
+        double[] num = countVowelsAndConsonants();
+        if ((num[0] / num[1]) < 0.33) {
+            vowelBid = Math.min(vowelBid + 1, maxVowelBid);
+        } else if (vowelBid > minVowelBid) {
+            vowelBid = Math.max(vowelBid - 1, minVowelBid);
         }
 
-        if ((numVowels / numConsonants) < 0.25) {
-            if (vowelBid < maxVowelBid) {
-                vowelBid += 1;
-            }
-        }
+        ArrayList<Letter> secretLetters = secretstate.getSecretLetters();
 
         Character bidChar = bidLetter.getCharacter();
-        int numChar = countLetterList(myLetters, bidChar);
+        int numChar = countCharacterList(myLetters, bidChar) + countLetterArrayList(secretLetters, bidLetter);
 
         myLetters.add(bidChar);
         String possibleBest = returnWord();
@@ -119,17 +94,16 @@ public class Group5Player extends words.core.Player {
         //if we need this character to make the best word given our current letters
         if (possibleBest.contains(bidChar.toString()) && numChar < numCharInBest) {
             if (possibleBest.length() >= 7) {
-                bid = 15;
-            }
-            else {
-                bid = ScrabbleValues.getWordScore(possibleBest) / 3 + 1;
+                bid = 20;
+            } else {
+                bid = ScrabbleValues.getWordScore(possibleBest) / 2 + 1;
             }
         }
-        else if (isVowel(bidChar)) {
+        else if (isVowel(bidChar) && !myLetters.contains(bidChar) && !secretLetters.contains(bidLetter)) {
             bid = vowelBid;
         }
         else {
-            bid = bidLetter.getValue();
+            bid = 6;
         }
 
         return Math.min(bid, secretstate.getScore());
